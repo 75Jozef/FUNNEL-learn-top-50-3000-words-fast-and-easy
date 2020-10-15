81178c69c232f6c9d3b7b4bed00a84f2b63d5485
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-10-16 00:11:12 +0200

    construct word done

--- models/wordsToLearn.js
+++ models/wordsToLearn.js
@@ -1,13 +1,267 @@
 class wordToLearn {
-  constructor(wordLevel, id, word, description, wordLength) {
-    this.id = id;
-    this.word = word;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this.wordLevel = wordLevel;
+  constructor(
+    userlvl,
+    iden,
+    sten,
+    English,
+    idit,
+    stit,
+    Italian,
+    idpa,
+    stpa,
+    Punjabi,
+    idjv,
+    stjv,
+    Javanese,
+    idth,
+    stth,
+    Thai,
+    idpl,
+    stpl,
+    Polish,
+    idfa,
+    stfa,
+    Persian,
+    idur,
+    stur,
+    Urdu,
+    iduk,
+    stuk,
+    Ukrainian,
+    idro,
+    stro,
+    Romanian,
+    idnl,
+    stnl,
+    Dutch,
+    idsr,
+    stsr,
+    Serbian,
+    idhr,
+    sthr,
+    Croatian,
+    idhu,
+    sthu,
+    Hungarian,
+    idel,
+    stel,
+    Greek,
+    idcs,
+    stcs,
+    Czech,
+    idsv,
+    stsv,
+    Swedish,
+    idbe,
+    stbe,
+    Belarusian,
+    idfi,
+    stfi,
+    Finnish,
+    idda,
+    stda,
+    Danish,
+    idcn,
+    stcn,
+    Chinese_Simplified,
+    ides,
+    stes,
+    Spanish,
+    idar,
+    star,
+    Arabic,
+    idpt,
+    stpt,
+    Portuguese_Brasil,
+    idid,
+    stid,
+    Indonesian,
+    idms,
+    stms,
+    Malay,
+    idfr,
+    stfr,
+    French,
+    idru,
+    stru,
+    Russian,
+    idde,
+    stde,
+    German,
+    idja,
+    stja,
+    Japanese,
+    idhi,
+    sthi,
+    Hindi,
+    idbn,
+    stbn,
+    Bengali,
+    idtr,
+    sttr,
+    Turkish,
+    idmr,
+    stmr,
+    Marathi,
+    idko,
+    stko,
+    Korean,
+    idvi,
+    stvi,
+    Vietnamese,
+    idta,
+    stta,
+    Tamil,
+    idsk,
+    stsk,
+    Slovak,
+    idaf,
+    staf,
+    Afrikaans,
+    idlv,
+    stlv,
+    Latvian,
+    ideo,
+    steo,
+    Esperanto,
+    idet,
+    stet,
+    Estonian,
+    idbg,
+    stbg,
+    Bulgarian
+  ) {
+    this.userlvl = userlvl;
+    this.iden = iden;
+    this.sten = sten;
+    this.English = English;
+    this.idit = idit;
+    this.stit = stit;
+    this.Italian = Italian;
+    this.idpa = idpa;
+    this.stpa = stpa;
+    this.Punjabi = Punjabi;
+    this.idjv = idjv;
+    this.stjv = stjv;
+    this.Javanese = Javanese;
+    this.idth = idth;
+    this.stth = stth;
+    this.Thai = Thai;
+    this.idpl = idpl;
+    this.stpl = stpl;
+    this.Polish = Polish;
+    this.idfa = idfa;
+    this.stfa = stfa;
+    this.Persian = Persian;
+    this.idur = idur;
+    this.stur = stur;
+    this.Urdu = Urdu;
+    this.iduk = iduk;
+    this.stuk = stuk;
+    this.Ukrainian = Ukrainian;
+    this.idro = idro;
+    this.stro = stro;
+    this.Romanian = Romanian;
+    this.idnl = idnl;
+    this.stnl = stnl;
+    this.Dutch = Dutch;
+    this.idsr = idsr;
+    this.stsr = stsr;
+    this.Serbian = Serbian;
+    this.idhr = idhr;
+    this.sthr = sthr;
+    this.Croatian = Croatian;
+    this.idhu = idhu;
+    this.sthu = sthu;
+    this.Hungarian = Hungarian;
+    this.idel = idel;
+    this.stel = stel;
+    this.Greek = Greek;
+    this.idcs = idcs;
+    this.stcs = stcs;
+    this.Czech = Czech;
+    this.idsv = idsv;
+    this.stsv = stsv;
+    this.Swedish = Swedish;
+    this.idbe = idbe;
+    this.stbe = stbe;
+    this.Belarusian = Belarusian;
+    this.idfi = idfi;
+    this.stfi = stfi;
+    this.Finnish = Finnish;
+    this.idda = idda;
+    this.stda = stda;
+    this.Danish = Danish;
+    this.idcn = idcn;
+    this.stcn = stcn;
+    this.Chinese_Simplified = Chinese_Simplified;
+    this.ides = ides;
+    this.stes = stes;
+    this.Spanish = Spanish;
+    this.idar = idar;
+    this.star = star;
+    this.Arabic = Arabic;
+    this.idpt = idpt;
+    this.stpt = stpt;
+    this.Portuguese_Brasil = Portuguese_Brasil;
+    this.idid = idid;
+    this.stid = stid;
+    this.Indonesian = Indonesian;
+    this.idms = idms;
+    this.stms = stms;
+    this.Malay = Malay;
+    this.idfr = idfr;
+    this.stfr = stfr;
+    this.French = French;
+    this.idru = idru;
+    this.stru = stru;
+    this.Russian = Russian;
+    this.idde = idde;
+    this.stde = stde;
+    this.German = German;
+    this.idja = idja;
+    this.stja = stja;
+    this.Japanese = Japanese;
+    this.idhi = idhi;
+    this.sthi = sthi;
+    this.Hindi = Hindi;
+    this.idbn = idbn;
+    this.stbn = stbn;
+    this.Bengali = Bengali;
+    this.idtr = idtr;
+    this.sttr = sttr;
+    this.Turkish = Turkish;
+    this.idmr = idmr;
+    this.stmr = stmr;
+    this.Marathi = Marathi;
+    this.idko = idko;
+    this.stko = stko;
+    this.Korean = Korean;
+    this.idvi = idvi;
+    this.stvi = stvi;
+    this.Vietnamese = Vietnamese;
+    this.idta = idta;
+    this.stta = stta;
+    this.Tamil = Tamil;
+    this.idsk = idsk;
+    this.stsk = stsk;
+    this.Slovak = Slovak;
+    this.idaf = idaf;
+    this.staf = staf;
+    this.Afrikaans = Afrikaans;
+    this.idlv = idlv;
+    this.stlv = stlv;
+    this.Latvian = Latvian;
+    this.ideo = ideo;
+    this.steo = steo;
+    this.Esperanto = Esperanto;
+    this.idet = idet;
+    this.stet = stet;
+    this.Estonian = Estonian;
+    this.idbg = idbg;
+    this.stbg = stbg;
+    this.Bulgarian = Bulgarian;
   }
 }
 
 export default wordToLearn;
-
-new Word("userlvl",iden,sten,"English",idit,stit,"Italian",idpa,stpa,"Punjabi",idjv,stjv,"Javanese",idth,stth,"Thai",idpl,stpl,"Polish",idfa,stfa,"Persian",idur,stur,"Urdu",iduk,stuk,"Ukrainian",idro,stro,"Romanian",idnl,stnl,"Dutch",idsr,stsr,"Serbian",idhr,sthr,"Croatian",idhu,sthu,"Hungarian",idel,stel,"Greek",idcs,stcs,"Czech",idsv,stsv,"Swedish",idbe,stbe,"Belarusian",idfi,stfi,"Finnish",idda,stda,"Danish",idcn,stcn,"Chinese_Simplified",ides,stes,"Spanish",idar,star,"Arabic",idpt,stpt,"Portuguese_Brasil",idid,stid,"Indonesian",idms,stms,"Malay",idfr,stfr,"French",idru,stru,"Russian",idde,stde,"German",idja,stja,"Japanese",idhi,sthi,"Hindi",idbn,stbn,"Bengali",idtr,sttr,"Turkish",idmr,stmr,"Marathi",idko,stko,"Korean",idvi,stvi,"Vietnamese",idta,stta,"Tamil",idsk,stsk,"Slovak",idaf,staf,"Afrikaans",idlv,stlv,"Latvian",ideo,steo,"Esperanto",idet,stet,"Estonian",idbg,stbg,"Bulgarian"),
