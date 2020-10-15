da8a52f4f59baae60aca9d732b92e46ba4bb28c2
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-10-15 23:45:22 +0200

    words imported

---
+++ models/wordsToLearn.js
@@ -0,0 +1,13 @@
+class wordToLearn {
+  constructor(wordLevel, id, word, description, wordLength) {
+    this.id = id;
+    this.word = word;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.wordLevel = wordLevel;
+  }
+}
+
+export default wordToLearn;
+
+new Word("userlvl",iden,sten,"English",idit,stit,"Italian",idpa,stpa,"Punjabi",idjv,stjv,"Javanese",idth,stth,"Thai",idpl,stpl,"Polish",idfa,stfa,"Persian",idur,stur,"Urdu",iduk,stuk,"Ukrainian",idro,stro,"Romanian",idnl,stnl,"Dutch",idsr,stsr,"Serbian",idhr,sthr,"Croatian",idhu,sthu,"Hungarian",idel,stel,"Greek",idcs,stcs,"Czech",idsv,stsv,"Swedish",idbe,stbe,"Belarusian",idfi,stfi,"Finnish",idda,stda,"Danish",idcn,stcn,"Chinese_Simplified",ides,stes,"Spanish",idar,star,"Arabic",idpt,stpt,"Portuguese_Brasil",idid,stid,"Indonesian",idms,stms,"Malay",idfr,stfr,"French",idru,stru,"Russian",idde,stde,"German",idja,stja,"Japanese",idhi,sthi,"Hindi",idbn,stbn,"Bengali",idtr,sttr,"Turkish",idmr,stmr,"Marathi",idko,stko,"Korean",idvi,stvi,"Vietnamese",idta,stta,"Tamil",idsk,stsk,"Slovak",idaf,staf,"Afrikaans",idlv,stlv,"Latvian",ideo,steo,"Esperanto",idet,stet,"Estonian",idbg,stbg,"Bulgarian"),
