219cb51ae01eb01afa791499152213dbf96a9fd5
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-11-26 21:36:38 +0100

    ...

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,26 +2,18 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { SideNavigator } from './../navigation/ScreenNavigator';
+import { View, Text } from 'react-native';
 
 import * as wordsActions from '../store/actions/words';
 
 const AppNavigator = (props) => {
-  // preload of statuses
-
   const dispatch = useDispatch();
 
-  const [error, setError] = useState();
   const [isFetched, setIsFetched] = useState(false);
 
   const loadUserStatuses = useCallback(async () => {
-    setError(null);
-
-    try {
-      dispatch(wordsActions.loadStatusesFromDb());
-    } catch (err) {
-      setError(err.message);
-    }
-  }, [dispatch, setError, setIsFetched]);
+    dispatch(wordsActions.loadStatusesFromDb());
+  }, []);
 
   //once component loaded, it fires action to fetch user products from firebase by GET method; makes only once when building screen or when change is done on this screen (not non server)
 
@@ -29,13 +21,21 @@ const AppNavigator = (props) => {
     loadUserStatuses().then(() => {
       setIsFetched(true);
     });
-  }, [loadUserStatuses]);
+  }, []);
 
   // end of preload of statuses
 
   let isAuth = true;
   return (
-    <NavigationContainer>{isFetched && <SideNavigator />}</NavigationContainer>
+    <NavigationContainer>
+      {isFetched ? (
+        <SideNavigator />
+      ) : (
+        <View>
+          <Text>Failed loading statuses</Text>
+        </View>
+      )}
+    </NavigationContainer>
   );
 };
 
